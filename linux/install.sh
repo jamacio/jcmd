f8981ba9fb27438479f7e964927cd51babae6bb6
jamacio <jamacio@hotmail.com>
2024-02-17 17:32:18 -0300

    fixed error do add two or more commands in line

--- linux/install.sh
+++ linux/install.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
 
-cat << EOF > ~/jcmd
+cat << EOF > /usr/local/bin/jcmd
 #!/bin/bash
 
 add() {
     command_name=\$1
     shift
-    command_value=\$@
-    echo "\$command_name=\\\"\$command_value\\\"" >> ~/.jcmds/commands.conf
+    command_value="\$@"
+    echo "\$command_name=\"\$command_value\"" >> \$HOME/.jcmds/commands.conf
     echo "Command '\$command_name' added successfully!"
 }
 
 rm() {
     command_name=\$1
-    grep -v "\$command_name=" ~/.jcmds/commands.conf > ~/.jcmds/commands.conf.tmp
-    mv ~/.jcmds/commands.conf.tmp ~/.jcmds/commands.conf
-    echo "Command '\$command_name' removed successfully!"
+    grep -v "\$command_name=" \$HOME/.jcmds/commands.conf > \$HOME/.jcmds/commands.conf.tmp
+    mv \$HOME/.jcmds/commands.conf.tmp \$HOME/.jcmds/commands.conf
 }
 
 list() {
     echo "Available commands:"
-    cat ~/.jcmds/commands.conf | cut -d'=' -f1
+    cat \$HOME/.jcmds/commands.conf | cut -d'=' -f1
 }
 
 version() {
   echo "v0.0.1"
 }
 
-if [ ! -d "~/.jcmds" ]; then
-    mkdir -p ~/.jcmds
+if [ ! -d "\$HOME/.jcmds" ]; then
+    mkdir -p \$HOME/.jcmds
 fi
 
-if [ ! -f "~/.jcmds/commands.conf" ]; then
-    echo 'hello="echo '\''Hello World'\''"' > ~/.jcmds/commands.conf
+if [ ! -f "\$HOME/.jcmds/commands.conf" ]; then
+    echo 'hello="echo '\''Hello World'\''"' > \$HOME/.jcmds/commands.conf
 fi
 
-source ~/.jcmds/commands.conf
+source \$HOME/.jcmds/commands.conf
 
 command=\$1
 if [ "\$command" = "" ]; then
     list
 elif [ "\$command" = "add" ]; then
+    rm \$2
     add \$2 \$3
 elif [ "\$command" = "rm" ]; then
     rm \$2
+    echo "Command '\$command_name' removed successfully!"
 elif [ "\$command" = "list" ]; then
    list
 elif [ "\$command" = "version" ]; then
@@ -56,6 +57,6 @@ else
 fi
 EOF
 
-chmod +x ~/jcmd
+chmod +x /usr/local/bin/jcmd
 
 echo "The 'jcmd' command has been successfully installed!"
